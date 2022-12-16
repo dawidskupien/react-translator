8f76342f7d2a47f85484ca8ba84bcdaf01812246
Dawid Skupień <oktoktoper@gmail.com>
2022-12-16 23:49:55 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="">
+      <div>
+        co tu robi fajna pani feta jak rakietawidaiwd awoidjawodjoaiwjdoi
+      </div>
+    </div>
+  );
+}
+
+export default App;
